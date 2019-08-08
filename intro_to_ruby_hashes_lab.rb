2231a3e94332c7c74ff5cab18ed1a8f467743410
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 16:43:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,53 @@
 def new_hash
-  # return an empty hash
+  {
+  }
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  dolphin = {
+    name = bottlenose
+    color = grey
+  }
+
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    name = Grace Hopper
+  }
+
+  person [:name] = "Clark"
+
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_me {
+    name = Christina
+    eyee = hazel
+  }
+
+  person [:id] = "29"
+
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  animals {
+    species = horse
+    breed = pinto
+  }
+
+  large_animal [:key] = "paint"
+
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  read {
+    name = Harry Potter
+    title = wizard
+  }
+
+  pet = "Hedwig"
+
 end
 
 def update_counting_hash(hash, key)
